aba88d7579ef0a220829ad7b811b5167efe39c0e
Tatyana <Tatyanatatyana777M@gmail.com>
2025-03-31 23:35:27 +0200

    has changed

---
+++ lesson_14_homework/script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('movie-form');
+const moviesGrid = document.getElementById('movies-grid');
+const clearButton = document.getElementById('clear-list');
+let movies = [];
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const year = document.getElementById('year').value;
+    const poster = document.getElementById('poster').value;
+    })
+
+    function createList() {
+        taskList.map((el) => {
+          const li = document.createElement("li");
+          li.innerText = `${el.title}: ${el.author}: ${el.year}: ${el.poster}`;
+          
+          li.onclick = changeStatus
+         
+          ul.append(li);
+        });
+
+    const movie = { title, author, year, poster };
+    movies.push(movie);
+    renderMovies();
+    form.reset();
+}
+
+clearButton.addEventListener('click', function() {
+    movies = [];
+    renderMovies();
+});
+
+function deleteMovie(index) {
+    movies.splice(index, 1);
+    renderMovies();
+}
